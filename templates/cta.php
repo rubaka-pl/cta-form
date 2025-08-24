b140bb258262c1f5a0b242af3f99236c2aaeedbd
Daniel <91855245+rubaka-pl@users.noreply.github.com>
2025-08-24 11:41:30 +0200

    Update cta.php

--- templates/cta.php
+++ templates/cta.php
@@ -19,7 +19,7 @@ $assets_base = plugins_url('assets/', CTA_CF_PLUGIN_FILE);
 
     <img class="avatar avatar--mid"
         src="<?php echo esc_url($assets_base . 'img/avatar-1.png'); ?>"
-        alt="аватар усміхненого чоловіка">
+        alt="аватар усміхненої жінки">
 
     <img class="decor-img"
         src="<?php echo esc_url($assets_base . 'img/background-sprite.svg'); ?>"
